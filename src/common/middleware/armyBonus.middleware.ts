6c469d75fe58b45cde1fa9b2e5a436b923f2345e
Aleks Markovic <aleks.markovic182@gmail.com>
2021-10-25 09:31:40 +0200

    Add request data check

--- src/common/middleware/armyBonus.middleware.ts
+++ src/common/middleware/armyBonus.middleware.ts
@@ -1,9 +1,21 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NestMiddleware,
+} from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
 @Injectable()
 export class ArmyBonusMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
+    if (Object.keys(req.query).length === 0) {
+      throw new HttpException(
+        { status: HttpStatus.BAD_REQUEST, error: 'No army data!' },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
     req.body.armies = [];
 
     Object.values(req.query).forEach((numberOfSoldiers: string) => {
